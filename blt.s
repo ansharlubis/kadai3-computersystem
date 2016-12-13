a88f87f6582da92712a2f3f6a808c3e544012d67
Luthfan Anshar Lubis <lubis.l.aa@m.titech.ac.jp>
2016-12-13 18:08:05 +0900

    maigudnes malasnya buat report

--- blt.s
+++ blt.s
@@ -9,16 +9,16 @@ main:
 	li $t1, 4
 	li $t2, 6
 	slt $t0, $t1, $t2		# check $t1 < $t2
-	beq $t0, $zero, label	# if $t1 < $ t2 jump to label
+	bne $t0, $zero, label	# if $t1 < $ t2 jump to label
 	li $v0, 4				
-	la $a0, A
-	syscall					# print true
+	la $a0, B
+	syscall					# print false
 	j end
 
 label:
 	li $v0, 4
-	la $a0, B
-	syscall					# print false
+	la $a0, A
+	syscall					# print true
 	
 end:
 	jr $ra
